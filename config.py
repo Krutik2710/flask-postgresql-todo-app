6602c7c989b578b1ef42461eeeadcb167015f8dd
Krutik2710 <krutiku2710@gmail.com>
2026-07-20 23:25:13 +0530

    First Step towards containerization

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:1234@localhost:5432/todo_db"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:MyPassword123@localhost:5432/todo_db"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
